24ab6f945d4413eabbd246b0aec569a0e052d923
poran <poran.cse@gmail.com>
2023-10-09 18:11:44 +0600

    configure project based on MVVM architecture

--- app/src/main/java/com/windnews/ui/MainActivity.kt
+++ app/src/main/java/com/windnews/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.windnews
+package com.windnews.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.windnews.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
