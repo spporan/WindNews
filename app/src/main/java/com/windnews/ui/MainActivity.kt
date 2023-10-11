0992a822a45edf6b8267ee642dafd4a04fccbaa7
poran <poran.cse@gmail.com>
2023-10-11 11:45:49 +0600

    Add Toolbar in MainActivity.kt and add network permission in manifest

--- app/src/main/java/com/windnews/ui/MainActivity.kt
+++ app/src/main/java/com/windnews/ui/MainActivity.kt
@@ -2,11 +2,38 @@ package com.windnews.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.windnews.R
+import com.windnews.databinding.ActivityMainBinding
+import com.windnews.ui.fragments.SplashFragment
+import com.windnews.ui.viewmodels.NewsListViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        binding.toolbar.setNavigationOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+        savedInstanceState ?: openSplash()
+    }
+    
+    private fun openSplash() {
+
+        val splashFragment = SplashFragment.newInstance()
+
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.container, splashFragment, SplashFragment.TAG)
+            .commit()
     }
 }
